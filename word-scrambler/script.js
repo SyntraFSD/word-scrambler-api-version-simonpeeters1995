5bb00392989817757bb8aa1c1141e2dbe62f2ffb
simonpeeters1995 <simsimpeeeters@gmail.com>
2018-10-19 11:23:01 +0200

    word-scrambler poging

--- word-scrambler/script.js
+++ word-scrambler/script.js
@@ -1,1 +1,16 @@
 console.log('ready to go!!');
+
+//select textarea
+let tekst = document.querySelector(".textarea");
+//select btn
+let button = document.querySelector("#submit");
+
+console.log(tekst.textContent);
+//
+button.addEventListener("click",function(){
+let array = tekst.textContent.split(" ");
+console.log(array); 
+
+
+let uitkomst = document.querySelector(".card-content");
+});
