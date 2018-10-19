f7b6ab9696a28891fed87959ebf19b272b9fa35c
quinroos <quinroos@student.arteveldehs.be>
2018-10-19 12:03:08 +0200

    made the words in wordwrap display in a random pos

--- word-scrambler/script.js
+++ word-scrambler/script.js
@@ -1,7 +1,15 @@
 console.log('ready to go!!');
 
 //select textarea
-let textarea = document.querySelector('.textarea');
+let textarea = document.querySelector('textarea');
 
 //select button
 let button = document.querySelector('#submit');
+    //console.log(textarea.textContent);
+    button.addEventListener("click", function(){
+    let words = textarea.innerHTML.split(' ');
+    console.log(words);
+    words.sort(function(a, b){return 0.5 - Math.random()});
+    document.getElementById('card').innerHTML = words;
+});
+
