83d29606bc1c16e942b27687e5af1eee91113f63
simonpeeters1995 <simsimpeeeters@gmail.com>
2018-10-19 12:16:01 +0200

    vervollediging textwrapper

--- word-scrambler/script.js
+++ word-scrambler/script.js
@@ -1,17 +1,23 @@
 console.log('ready to go!!');
 
 //select textarea
-let tekst = document.querySelector(".textarea");
+let tekst = document.querySelector("textarea");
 //select btn
 let button = document.querySelector("#submit");
 
 console.log(tekst.textContent);
 //
 button.addEventListener("click",function(){
-let array = tekst.textContent.split(' ');
-for(let i =0;i<array.length;i++){
 
+let wordArray = tekst.innerHTML.split(' ');
+let textChanged="";
+
+wordArray.sort(function(a,b) {return 0.5-Math.random()});
+for(let i =0;i<wordArray.length;i++){
+ textChanged += " " + wordArray[i];
+ console.log(textChanged);
 }
 
-let uitkomst = document.querySelector(".card-content");
+document.getElementById('card').innerHTML=textChanged;
+
 });
