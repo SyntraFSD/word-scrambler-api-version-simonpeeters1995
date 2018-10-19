3a464cdcc4ac7ed07912cfbf563013ccb10d2d52
Korneel Eeckhout <korneel@pienter.tech>
2018-10-19 11:21:50 +0200

    Selectors

--- word-scrambler/script.js
+++ word-scrambler/script.js
@@ -1,8 +1,13 @@
 console.log('ready to go!!');
 
-//select textearea
+//select textarea
 let textarea = document.querySelector('textarea');
 //select btn
 let btn = document.querySelector('#submit');
 
+btn.addEventListener('click',function(){
 
+});
+
+
+console.log(textarea.textContent);
