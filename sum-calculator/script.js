f7cf3b061cf4376e2078e59a6ba29ce5b19c6470
simonpeeters1995 <simsimpeeeters@gmail.com>
2018-10-22 11:56:06 +0200

    functie sum in sumOefening

--- sum-calculator/script.js
+++ sum-calculator/script.js
@@ -6,8 +6,9 @@ button.addEventListener("click",sum())
 
 function sum() {
 
-let number1 = document.querySelector(".input1");
-let number2 = document.querySelector(".input2");
+let number1 = document.querySelector(".input1").value;
+let number2 = document.querySelector(".input2").value;
 
-
+let valueSum = parseInt(number1)+parseInt(number2);
+document.getElementById("result").innerHTML=valueSum;
 }
