2e2abd42c274f86a0e42c0149f077e22a52dbd92
simonpeeters1995 <simsimpeeeters@gmail.com>
2018-10-22 11:47:59 +0200

    begin oefening sum

--- sum-calculator/script.js
+++ sum-calculator/script.js
@@ -1,1 +1,13 @@
 console.log('ready to go!!');
+
+let button= document.querySelector(".button");
+
+button.addEventListener("click",sum())
+
+function sum() {
+
+let number1 = document.querySelector(".input1");
+let number2 = document.querySelector(".input2");
+
+
+}
