cc8e82ca059618efd56f1bfb0d95d4054a1f6c4b
quinroos <quinroos@student.arteveldehs.be>
2018-10-22 11:22:41 +0200

    made the scramble work with custom value

--- word-scrambler/script.js
+++ word-scrambler/script.js
@@ -3,21 +3,22 @@ console.log('ready to go!!');
 //select textarea
 let textarea = document.querySelector('textarea');
 
-//select button
-let button = document.querySelector('#submit');
+ //select button
+ let button = document.querySelector('#submit');
+     //console.log(textarea.textContent);
 
-/*     button.addEventListener("click", function(){
-    let words = textarea.value;
-    words.innerHTML.split(' ');
-    console.log(words);
+/* button.addEventListener("click", function(){
+     let words = textarea.value.split(' ');
+     console.log(words);
+     words.sort(function(a, b){return 0.5 - Math.random()});
+     document.getElementById('card').innerHTML = words;
+ }); */
+
+
+function clicker(){
+    let words = textarea.value.split(' ');
+    document.getElementById('card').innerHTML = words;
     words.sort(function(a, b){return 0.5 - Math.random()});
     document.getElementById('card').innerHTML = words;
-}); */
-function clicker(){
-    let words = textarea.value;
-    words.split(' ');
-    //words.sort(function(a, b){return 0.5 - Math.random()});
-    document.getElementById('card').innerHTML = words;
-    console.log(words);
 }
 button.addEventListener('click', clicker);
