739672d8b25033180bf006ee0e99731717773bb0
quinroos <quinroos@student.arteveldehs.be>
2018-10-22 11:52:01 +0200

    addition of 2 numbers

--- sum-calculator/script.js
+++ sum-calculator/script.js
@@ -1,1 +1,10 @@
 console.log('ready to go!!');
+let number1 = document.querySelector('#number-01');
+let number2 = document.querySelector('#number-02');
+let button = document.querySelector('.is-info');
+
+function clicker(){
+    let calc = parseInt(number1.value) + parseInt(number2.value);
+    document.getElementById('result').innerHTML = calc;
+}
+button.addEventListener('click', clicker);
