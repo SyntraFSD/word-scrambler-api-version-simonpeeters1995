1fc1b339d09f828c0727d91aba7e4dcb7f0b7717
DANNY EERAERTS <danny@MacBook-Pro-van-DANNY.local>
2018-10-23 22:38:31 +0200

    Nog een fout verbeterd

--- sum-calculator/script.js
+++ sum-calculator/script.js
@@ -4,14 +4,14 @@ let knop = document.querySelector(".button-is-info")
 knop.addEventListener('click', calculate);
 
 function calculate () {
-    let snumber_01 = document.querySelector ("#number-01").nodeValue;
-    let snumber_02 = document.querySelector ("#number-02").nodeValue;
+    let snumber_01 = document.querySelector ("#number-01").value;
+    let snumber_02 = document.querySelector ("#number-02").value;
     let isom = parseInt(snumber_01.value) + parseInt(snumber_02.value);
     let iverschil = parseInt(snumber_01.value) - parseInt(snumber_02.value);
     let iproduct = parseInt(snumber_01.value) * parseInt(snumber_02.value);
-    let iverschil = parseInt(snumber_01.value) / parseInt(inumber_02.value);
-    let fomzetting = parseFloat(verschil);
-    let iresultaat = Math.round(verschil*10)/10;
+    let ideling = parseInt(snumber_01.value) / parseInt(inumber_02.value);
+    let fomzetting = parseFloat(ideling);
+    let iresultaat = Math.round(fomzetting*10)/10;
     document.getElementById("#result1").innerHTML = isom;
     document.getElementById("#result2").innerHTML = iverschil;
     document.getElementById("#result3").innerHTML = iproduct;
