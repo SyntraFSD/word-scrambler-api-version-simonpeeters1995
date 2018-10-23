f5ae0207f59d198c5b488cd79bba21d07c6d0890
DANNY EERAERTS <danny@MacBook-Pro-van-DANNY.local>
2018-10-23 13:07:16 +0200

    added the exemple from the class

--- word-scrambler/script.js
+++ word-scrambler/script.js
@@ -1,1 +1,13 @@
 console.log('ready to go!!');
+
+//select textarea
+let textarea = document.querySelector('textarea');
+//select btn
+let btn = document.querySelector('#submit');
+
+btn.addEventListener('click',clicker);
+
+function clicker(){
+    let textContent = textarea.textValue;
+    console.log(textContent.split(' '));
+}
