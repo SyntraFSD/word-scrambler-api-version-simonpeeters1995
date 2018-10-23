f485011ae42ca635aaa1061a3baf93fdf78d282c
DANNY EERAERTS <danny@MacBook-Pro-van-DANNY.local>
2018-10-23 21:58:57 +0200

    functie calculation afgerond

--- sum-calculator/script.js
+++ sum-calculator/script.js
@@ -1,1 +1,22 @@
 console.log('ready to go!!');
+
+let knop = document.querySelector(".button-is-info")
+knop.addEventListener('click', calculate);
+
+function calculate () {
+    let snumber_01 = document.querySelector ("#number-01").nodeValue;
+    let snumber_02 = document.querySelector ("#number-02").nodeValue;
+    let isom = parseInt(snumber_01.value) + parseInt(snumber_02.value);
+    let iverschil = parseInt(snumber_01.value) - parseInt(snumber_02.value);
+    let iproduct = parseInt(snumber_01.value) * parseInt(snumber_02.value);
+    let iverschil = parseInt(snumber_01.value) / parseInt(inumber_02.value);
+    let fomzetting = parseFloat(verschil);
+    let iresultaat = Math.round(verschil*10)/10;
+    document.getElementById('result').innerHTML = isom;
+    document.getElementById('result2').innerHTML = iverschil;
+    document.getElementById('result3').innerHTML = iomzetting;
+    document.getElementById('result4').innerHTML = iresultaat;
+}
+
+
+
