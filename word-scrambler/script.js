085759067b3bf953bb4a0101d599314a4ca694f5
Your Name <you@example.com>
2018-10-24 13:10:43 +0200

    WordCommit1

--- word-scrambler/script.js
+++ word-scrambler/script.js
@@ -1,1 +1,22 @@
 console.log('ready to go!!');
+
+//select textarea
+let textarea = document.querySelector(".textarea");
+
+//select button
+let btn = document.querySelector("#submit");
+
+//document is root van uw DOM
+
+btn.addEventListener("click",clicker);    
+    
+function clicker(){
+    let Blablabla = textarea.value;
+
+    Blablabla = Blablabla.split(" ");
+    console.log(Blablabla);
+  
+};
+
+
+
