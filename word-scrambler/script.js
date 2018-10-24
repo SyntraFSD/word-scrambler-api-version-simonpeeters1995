9bd55ab0619c088fc00ba01b08491635a971892a
Thibault Zenner <thibault.zenner@gmail.com>
2018-10-24 13:11:01 +0200

    Renamed script.js to thibsie.js

--- word-scrambler/script.js
+++
@@ -1,8 +0,0 @@
-console.log('ready to go!!');
-
-//select text area
-let TextArea = document.querySelector("textarea");
-//select btn
-let btn = document.querySelector("#submit");
-
-btn.onclick = function();
---
+++ word-scrambler/thibsie.js
@@ -0,0 +1,13 @@
+//select text area
+let textarea = document.querySelector("textarea");
+//select btn
+let btn = document.querySelector("#submit");
+
+
+function clicker(){
+    let blablabla = textarea.value;
+    blablabla = blablabla.split(' ');
+    console.log(blablabla);
+}
+
+btn.addEventListener('click',clicker);
