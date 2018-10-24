d41843693010a47a3a2ba3d86e2edcf275d5868b
Your Name <you@example.com>
2018-10-24 15:53:44 +0200

    Antwoorden

--- ex01_word-scrambler/script.js
+++ ex01_word-scrambler/script.js
@@ -1,9 +1,9 @@
 //select textarea
-let userInput = document.querySelector(".textarea");
+let userInput = document.querySelector("#user-input");
 //select btn
 let submitBtn = document.querySelector("#submit-btn");
 //select result container
-let resultContainer = document.querySelector(".card-content");
+let resultContainer = document.querySelector("#result-container");
 //# select wordCount
 let wordCountContainer = document.querySelector("#word-count");
 //# select letterCount
@@ -12,45 +12,72 @@ let letterCountContainer = document.querySelector("#letter-count");
 
 function getUserInput() {
     //return value of userInput
-    return userInput.textContent;
+    return userInput.value;
 }
 
 function textToWordArray(text) {
     //return array of words
-    return text.split(" ");
+    return text.trim().split(" ");
 }
 
 function arrayToText(array) {
+    return array.join(" ");
 }
 
 function getRandomNumber(max) {
     //return random number between 0 and max (including 0 and excluding max)
+    Math floor(Math.random() * max);
     
 }       
 
 function scrambleArray(oldArray) {
     //return scrambled array
+    let newArray = [];
 
+    while(oldArray.length > 0){
+        let randomIndex = getRandomNumber(oldArray.length);
+        newArray.push(oldArray[randomIndex]);
+        oldArray.splice(randomIndex, 1); 
+}
+    return newArray;
 }
 
 function scrambleText(text) {
     // return scrambled text
+    return text.arrayToText(" ");
 }
+
+function printScrambledText(text){
+    resultContainer.textContent = text;
+}
+
 
 function onClickScramble() {
     // update textContent of resultContainer
+    let inputText = getUserInput();
+    let inputTextArray = textToWordArray(inputText);
+    let scrambleInputTextArray = scrambleArray(inputTextArray);
+    let scrambledInputText = arrayToText(scrambledInputText);
+    printScrambledText(scrambledInputText);
 }
 
 function realTimeScramble(event) {
     //## update textContent of resultContainer realtime
+    let inputText = getUserInput();
+    let inputTextArray = textToWordArray(inputText);
+    let scrambleInputTextArray = scrambleArray(inputTextArray);
+    let scrambledInputText = arrayToText(scrambledInputText);
+    printScrambledText(scrambledInputText);
 }
 
 function getWordCount(text) {
     //# return word count
+    
 }
 
 function getLetterCount(text) {
     //# return letter count
+    
 }
 
 function updateWordCount(wordCount) {
