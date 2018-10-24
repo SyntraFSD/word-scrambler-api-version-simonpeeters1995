88693388a354e28a1b0ea99ed760f98203d28d42
simonpeeters1995 <simsimpeeeters@gmail.com>
2018-10-24 13:08:11 +0200

    commit changes

--- sum-calculator/script.js
+++ sum-calculator/script.js
@@ -2,13 +2,13 @@ console.log('ready to go!!');
 
 let button= document.querySelector(".button");
 
-button.addEventListener("click",sum())
-
 function sum() {
 
-let number1 = document.querySelector(".input1").value;
-let number2 = document.querySelector(".input2").value;
-
-let valueSum = parseInt(number1)+parseInt(number2);
+let valueSum = parseInt(document.querySelector(".input1").value)
+ + parseInt(document.querySelector(".input2").value);
+ 
 document.getElementById("result").innerHTML=valueSum;
+
 }
+
+button.addEventListener("click",sum);
