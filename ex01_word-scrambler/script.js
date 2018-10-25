c911410fc3296a91711d3ed882d6fc4cc5644995
DANNY EERAERTS <danny@MacBook-Pro-van-DANNY.local>
2018-10-25 22:23:43 +0200

    Text update OK. Tellers Not OK

--- ex01_word-scrambler/script.js
+++ ex01_word-scrambler/script.js
@@ -5,7 +5,7 @@ let submitBtn = document.querySelector('#submit-btn');
 //select result container
 let resultContainer = document.querySelector('#result-container');
 //# select wordCount
-let wordCountContainer = document.querySelector('#word-Count')
+let wordCountContainer = document.querySelector('#word-Count');
 //# select letterCount
 let letterCountContainer =document.querySelector('#letter-count');
 
@@ -44,40 +44,52 @@ function scrambleArray(oldArray) {
 
 function scrambleText(text) {
     // return scrambled text
+    let woordenString = textToWordArray(text);
+    let woordenRij = scrambleArray(woordenString);
+    let gewijzigdeWoordenString = arrayToText(woordenRij);
+
+
+    return gewijzigdeWoordenString;
 }
 
 function printScrambledText(text){
     resultContainer.textContent = text;
 }
 
- {function onClickScramble()
+function onClickScramble() {
     // update textContent of resultContainer
     let inputText = getUserInput();
     let inputTextArray = textToWordArray(inputText);
     let scrambledInputTextArray = scrambleArray(inputTextArray);
     let scrambledInputText = arrayToText(scrambledInputTextArray);
     printScrambledText(scrambledInputText);
-
-}   console.log(getRandomNumber(10));
+}
 
 function realTimeScramble(event) {
     //## update textContent of resultContainer realtime
+    let nieuweTekst = getUserInput();
+    let gehaspeldeTekst = scrambleText(nieuweTekst);
+    resultContainer.textContent=gehaspeldeTekst;
 }
 
 function getWordCount(text) {
     //# return word count
+    return text.split(" ").length;
 }
 
 function getLetterCount(text) {
     //# return letter count
+    return text.length;
 }
 
 function updateWordCount(wordCount) {
     //# update the Word Count
+    wordCountContainer.textContent = wordCount;
 }
 
 function updateLetterCount(letterCount){
     //# update the Letter Count
+    letterCountContainer.textContent = letterCount;
 }
 
 function updateCounts(event) {
@@ -85,6 +97,10 @@ function updateCounts(event) {
     let currentText = this.value;
     // this when function is executed by event => event.target
     // this.value == event.target.value
+    let letterTeller = getletterCount(currentText);
+    let woordTeller = getWordCount(currentText);
+    updateWordCount(woordTeller);
+    updateLetterCount(letterTeller);
 }
 
 //add click event listener to submitBtn
