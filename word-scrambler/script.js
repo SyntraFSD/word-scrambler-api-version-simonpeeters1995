505e9de3b7c2ffb91b2c0ddb6803fe91a9f69994
Your Name <you@example.com>
2018-10-26 09:04:57 +0200

    26/10

--- word-scrambler/script.js
+++ word-scrambler/script.js
@@ -1,1 +1,17 @@
 console.log('ready to go!!');
+//select textarea
+
+let textarea = document.querySelector('#textarea');
+
+//select btn
+
+let btn = document.querySelector('#submit');
+
+function clicker(){
+    let textinhoud = textarea.innerText;
+    let charArray = textinhoud.split("");
+  
+
+console.log(textinhoud);
+}
+btn.addEventListener('click',clicker);
