10bbf89622b89d0beb52b507f903da3196552dbe
Korneel Eeckhout <korneel@pienter.tech>
2018-11-21 10:26:53 +0100

    empty exercise

--- ex01_word-scrambler/script.js
+++ ex01_word-scrambler/script.js
@@ -1,99 +1,72 @@
-// select textarea
-const userInput = document.querySelector('#user-input');
-// select btn
-const submitBtn = document.querySelector('#submit-btn');
-// select result container
-const resultContainer = document.querySelector('#result-container');
-// # select wordCount
-const wordCountContainer = document.querySelector('#word-count');
-// # select letterCount
-const letterCountContainer = document.querySelector('#letter-count');
+//select textarea
+let userInput;
+//select btn
+let submitBtn;
+//select result container
+let resultContainer;
+//# select wordCount
+let wordCountContainer;
+//# select letterCount
+let letterCountContainer;
+
 
 function getUserInput() {
-  // return value of userInput
-  return userInput.value;
+  //return value of userInput
 }
 
 function textToWordArray(text) {
-  // return array of words
-  return text.trim().split(' ');
+  //return array of words
 }
 
 function arrayToText(array) {
-  return array.join(' ');
 }
 
 function getRandomNumber(max) {
-  // return random number between 0 and max (including 0 and excluding max)
-  return Math.floor(Math.random() * max);
+  //return random number between 0 and max (including 0 and excluding max)
 }
 
 function scrambleArray(oldArray) {
-  // return scrambled array
-  const newArray = [];
-
-  while (oldArray.length > 0) {
-    const randomIndex = getRandomNumber(oldArray.length);
-    newArray.push(oldArray[randomIndex]);
-    oldArray.splice(randomIndex, 1);
-  }
-  return newArray;
+  //return scrambled array
 }
 
-function scrambleText(oldText) {
-  //  return scrambled text
-  const textArray = textToWordArray(oldText);
-  const scrambledArray = scrambleArray(textArray);
-  return arrayToText(scrambledArray);
+function scrambleText(text) {
+  // return scrambled text
 }
 
 function onClickScramble() {
-  //  update textContent of resultContainer
-  const userInputValue = getUserInput();
-  resultContainer.textContent = scrambleText(userInputValue);
+  // update textContent of resultContainer
 }
 
-function realTimeScramble() {
-  // ## update textContent of resultContainer realtime
-  const userInputValue = this.value;
-  resultContainer.textContent = scrambleText(userInputValue);
+function realTimeScramble(event) {
+  //## update textContent of resultContainer realtime
 }
 
 function getWordCount(text) {
-  // # return word count
-  const userInputArray = textToWordArray(text);
-  return userInputArray.length;
+  //# return word count
 }
 
 function getLetterCount(text) {
-  // # return letter count
-  return text.length;
+  //# return letter count
 }
 
 function updateWordCount(wordCount) {
-  // # update the Word Count
-  wordCountContainer.textContent = wordCount;
+  //# update the Word Count
 }
 
 function updateLetterCount(letterCount) {
-  // # update the Letter Count
-  letterCountContainer.textContent = letterCount;
+  //# update the Letter Count
 }
 
-function updateCounts() {
-  // # update Word Count and Letter Count
-  const currentText = this.value;
-  //  this when function is executed by event => event.target
-  //  this.value == event.target.value
-  const wordCount = getWordCount(currentText);
-  const letterCount = getLetterCount(currentText);
-  updateWordCount(wordCount);
-  updateLetterCount(letterCount);
+function updateCounts(event) {
+  //# update Word Count and Letter Count
+  let currentText = this.value;
+  // this when function is executed by event => event.target
+  // this.value == event.target.value
 }
 
-// add click event listener to submitBtn
-submitBtn.addEventListener('click', onClickScramble);
-// # add input event listener to userInput for counts
-userInput.addEventListener('input', updateCounts);
-// ## add input event listener to userInput for realTimeScramble
-userInput.addEventListener('input', realTimeScramble);
+//add click event listener to submitBtn
+submitBtn.addEventListener("click", onClickScramble);
+//# add input event listener to userInput for counts
+userInput.addEventListener("input", updateCounts);
+//## add input event listener to userinput for realTimeScramble
+userInput.addEventListener("input", realTimeScramble);
